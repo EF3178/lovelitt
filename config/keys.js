8b852929eee4099abf67666c614484816990fdc9
EF3178 <64409068+EF3178@users.noreply.github.com>
2021-01-05 15:40:38 +0100

    update keys

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+     MongoDB : 'mongodb+srv://lovelittapp:<password>@lovelitt.x6k5u.mongodb.net/<dbname>?retryWrites=true&w=majority',
+     FacebookAppID : '435994064247998',
+     FacebookAppSecret: 'b349fb8830f57219555848e6c009e257'
+}
